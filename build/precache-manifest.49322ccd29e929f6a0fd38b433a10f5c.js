707e3e78640410d70680d9eab28ebfab1c828ad0
attobra <nora.attobra@gmail.com>
2020-08-18 14:10:28 -0400

    add build

---
+++ build/precache-manifest.49322ccd29e929f6a0fd38b433a10f5c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8170b0aac0b86534924ea6a5558778be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0751ac552563581fd2c7",
+    "url": "/static/js/2.419013f1.chunk.js"
+  },
+  {
+    "revision": "e4fa17538b7ff90583b8",
+    "url": "/static/js/main.cf7631f2.chunk.js"
+  },
+  {
+    "revision": "b1cfa85f861ca69c3b05",
+    "url": "/static/js/runtime-main.941946b3.js"
+  }
+]);
